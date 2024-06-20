405d8ec08ceb6074a259db4a134d1c8802e00818
Jefferson Chavez Diaz <jefersonchavezdiaz.14@hotmail.com>
2024-06-20 10:51:36 -0500

    create a new file index-2.js

---
+++ js/index-2.js
@@ -0,0 +1,15 @@
+// CONTENIDO DINÁMICO EN JAVASCRIPT
+
+// Tipos de comillas en JavaScript <- SINGLE, DOUBLE, BACKTICK
+let name1 = 'JAB'
+let name2 = "JAB"
+let name3 = `JAB`
+let age = 18
+
+let result1 = 'My name is ' + name1 + ' and my age is ' + age
+let result2 = "My name is " + name2
+let result3 = `My name is ${name3} and my age is ${age}`
+
+console.log(result1) // My name is JAB and my age is 18
+console.log(result2) // My name is JAB
+console.log(result3) // My name is JAB and my age is 18
