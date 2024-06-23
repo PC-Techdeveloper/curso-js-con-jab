8be0495011dab96a494dcbc19187915f0ce8fda4
Jefferson Chavez Diaz <jefersonchavezdiaz.14@hotmail.com>
2024-06-22 20:27:26 -0500

    more content

--- js/index-5.js
+++ js/index-5.js
@@ -2,12 +2,21 @@
 
 /*
 keydown <- Se ejecuta cuando se presiona una tecla en el teclado.
-keyup <- Se ejecuta cuando se deja de presionar una tecla en el teclado.
+keyup <- Se ejecuta cuando suelta una tecla en el teclado.
 keypress <- Se ejecuta cuando se presiona una tecla en el teclado.
 */
-document.querySelector('.campo').addEventListener('keydown', escribir);
 
-//Valores del objeto del evento <- tecla pulsada (key) 🟨
-function escribir(e) {
-  console.log(e.key);
+//Utilidad del keypress
+// keyCode <- Obtiene el código de la tecla pulsada.
+// key <- Obtiene el nombre de la tecla pulsada.
+
+window.addEventListener('keypress', teclado);
+
+//Valores del objeto del evento <- tecla pulsada (key)
+// preventDefault() -> Anula la acción por defecto del evento.
+
+function teclado(e) {
+  //Como saber el nombre de una tecla (key)
+  console.log('>>' + e.keyCode);
+  e.key === 'ArrowUp' && e.type === 'keydown' ? (document.querySelector('.campo').style.backgroundColor = 'red') : (document.querySelector('.campo').style.backgroundColor = null);
 }
